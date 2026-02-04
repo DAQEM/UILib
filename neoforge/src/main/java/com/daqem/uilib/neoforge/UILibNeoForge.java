a7d8253b9c18c0a6de4a820c40bc3943b30f3201
DAQEM <66999025+DAQEM@users.noreply.github.com>
2026-02-04 19:09:19 +0100

    fix: neoforge package and class name

--- neoforge/src/main/java/com/daqem/uilib/neoforge/UILibNeoForge.java
+++ neoforge/src/main/java/com/daqem/uilib/neoforge/UILibNeoForge.java
@@ -1,12 +1,12 @@
-package com.daqem.uilib.forge;
+package com.daqem.uilib.neoforge;
 
 import com.daqem.uilib.UILib;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 
 @Mod(value = UILib.MOD_ID, dist = Dist.CLIENT)
-public class UILibForge {
+public class UILibNeoForge {
 
-    public UILibForge() {
+    public UILibNeoForge() {
     }
 }
