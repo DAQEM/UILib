527c72f54d319f3b51db71d0a861b5433376db11
DAQEM <66999025+DAQEM@users.noreply.github.com>
2026-05-10 16:24:34 +0200

    fix: scrollbar not dragable.

--- common/src/main/java/com/daqem/uilib/gui/widget/AbstractContainerWidget.java
+++ common/src/main/java/com/daqem/uilib/gui/widget/AbstractContainerWidget.java
@@ -8,6 +8,7 @@ import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.util.Optional;
 
 public abstract class AbstractContainerWidget extends AbstractScrollArea implements ContainerEventHandler {
     @Nullable
@@ -43,21 +44,59 @@ public abstract class AbstractContainerWidget extends AbstractScrollArea impleme
         this.focused = focused;
     }
 
+    @Override
     @Nullable
     public ComponentPath nextFocusPath(@NotNull FocusNavigationEvent focusNavigationEvent) {
-        return super.nextFocusPath(focusNavigationEvent);
+        return ContainerEventHandler.super.nextFocusPath(focusNavigationEvent);
     }
 
+    @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        return super.mouseClicked(mouseX, mouseY, button);
+        if (!this.active || !this.visible) {
+            return false;
+        }
+        boolean handled = false;
+        if (this.isValidClickButton(button)) {
+            if (this.updateScrolling(mouseX, mouseY, button)) {
+                handled = true;
+            } else {
+                Optional<GuiEventListener> optional = this.getChildAt(mouseX, mouseY);
+                if (optional.isPresent()) {
+                    GuiEventListener guiEventListener = optional.get();
+                    if (guiEventListener.mouseClicked(mouseX, mouseY, button)) {
+                        this.setFocused(guiEventListener);
+                        if (button == 0) {
+                            this.setDragging(true);
+                        }
+                        handled = true;
+                    }
+                } else {
+                    if (this.clicked(mouseX, mouseY)) {
+                        this.onClick(mouseX, mouseY);
+                        handled = true;
+                    }
+                }
+            }
+        }
+        return handled;
     }
 
+    @Override
     public boolean mouseReleased(double mouseX, double mouseY, int button) {
-        return super.mouseReleased(mouseX, mouseY, button);
+        boolean handled = super.mouseReleased(mouseX, mouseY, button);
+        return ContainerEventHandler.super.mouseReleased(mouseX, mouseY, button) || handled;
     }
 
+    @Override
     public boolean mouseDragged(double mouseX, double mouseY, int button, double dragX, double dragY) {
-        return super.mouseDragged(mouseX, mouseY, button, dragX, dragY);
+        if (this.scrolling) {
+            return super.mouseDragged(mouseX, mouseY, button, dragX, dragY);
+        }
+        boolean handled = ContainerEventHandler.super.mouseDragged(mouseX, mouseY, button, dragX, dragY);
+        if (!handled) {
+            handled = super.mouseDragged(mouseX, mouseY, button, dragX, dragY);
+        }
+        return handled;
     }
 
     public boolean isFocused() {
